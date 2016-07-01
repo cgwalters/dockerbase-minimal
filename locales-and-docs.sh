59656b2a4564b9807aa3fa12a16bdc60ef8018c8
Colin Walters <walters@verbum.org>
2016-07-01 18:55:24 -0400

    locales-and-docs: Also prune /usr/share/i18n

--- locales-and-docs.sh
+++ locales-and-docs.sh
@@ -2,7 +2,9 @@
 set -eu
 # See: https://bugzilla.redhat.com/show_bug.cgi?id=1051816
 KEEPLANG=en_US
-find /usr/share/locale -mindepth  1 -maxdepth 1 -type d -not -name "${KEEPLANG}" -exec rm -rf {} +
+for dir in locale i18n; do 
+    find /usr/share/${dir} -mindepth  1 -maxdepth 1 -type d -not \( -name "${KEEPLANG}" -o -name POSIX \) -exec rm -rf {} +
+done
 localedef --list-archive | grep -a -v ^"${KEEPLANG}" | xargs localedef --delete-from-archive
 mv -f /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
 build-locale-archive
