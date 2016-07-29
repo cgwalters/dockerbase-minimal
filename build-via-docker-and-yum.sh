89d1f201df2668ae661bb4ec22195119de9d9f98
Colin Walters <walters@verbum.org>
2016-07-29 16:53:12 -0400

    build: Port the builder invocation to python so we can parse arguments
    It's kind of clearer in shell script, except I hate parsing arguments
    in shell.

--- build-via-docker-and-yum.sh
+++ build-via-docker-and-yum.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 target=${1:-centos}
-case $target in
-    centos)
-	releasever=7 ;;
-    fedora)
-	releasever=24 ;;
-    *) echo "unknown target $target"; exit 1
-esac
+releasever=${2:-}
+if test -n ${releasever}; then
+    case $target in
+	centos)
+	    releasever=7 ;;
+	fedora)
+	    releasever=24 ;;
+	*) echo "unknown target $target"; exit 1
+    esac
+fi
 
 sed -e "s,\@target\@,${target}:${releasever},g" < buildcontainer/Dockerfile.in > buildcontainer/Dockerfile
 buildimgname=cgwalters/${target}-builder:${releasever}
 (cd buildcontainer && docker build -t ${buildimgname} .)
 
-docker run --privileged --net=host --rm -v $(pwd):/srv --workdir /srv ${buildimgname} /srv/build-via-yum.sh "${target}"
+docker run --privileged --net=host --rm -v $(pwd):/srv --workdir /srv ${buildimgname} /srv/build-via-yum --target "${target}" --releasever=${releasever}
